8dad24e6e323d7e0da30b456bc5a4ed6c34773ff
Nandini <nandinidevops3@gmail.com>
2022-03-28 11:08:28 +0530

    intial

---
+++ main.py
@@ -0,0 +1,8 @@
+def Add(a, b):
+        return a + b
+        
+def SayHello():
+        print("sup world from srcmake")
+        
+if _name_ == '_main_':
+        SayHello()
---
+++ main.test.py
@@ -0,0 +1,9 @@
+from main import Add
+
+def TestAdd():
+        assert Add(2,3) == 5
+        print('Add Function works correctly")
+        
+        
+if _name_ == '_main_':
+        TestAdd()
